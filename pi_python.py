31797bae403e31a6b24a33c9adb44ea8e73d21e7
Carlo Lepelaars <info@carlolepelaars.nl>
2023-11-18 22:05:47 +0100

    Clean up implementation

--- pi_python.py
+++ pi_python.py
@@ -1,10 +1,12 @@
 import mpmath 
 
-def pi_nth_digit(n):
+def pi_nth_digit(n: int) -> int:
     def pi_n(n):
         B2n = -2*n * mpmath.zeta(1 - 2*n)
         nominator = 2 * (-1)**(n+1) * mpmath.factorial(2 * n)
-        denominator = 2**(2 * n) * B2n * (1 - 2**(-2 * n)) * (1 - 3**(-2 * n)) * (1 - 5**(-2 * n)) * (1 - 7**(-2 * n) * (1 - 11**(-2 * n)))
+        denominator = 2**(2 * n) * B2n * (1 - 2**(-2 * n)) * \
+                    (1 - 3**(-2 * n)) * (1 - 5**(-2 * n)) * \
+                    (1 - 7**(-2 * n) * (1 - 11**(-2 * n)))
         return (nominator / denominator) ** (1 / (2 * n))
     if n < 0:
         raise ValueError("n must be a non-negative integer")
@@ -16,7 +18,7 @@ def pi_nth_digit(n):
         return 4
     else:
         pi_n_minus_1 = pi_n(n-1)
-        # Use mpmath for fractional part extraction
+        # Extract fractional part from pi_n_minus_1
         frac_part = mpmath.fmod(pi_n_minus_1 * mpmath.power(10, n - 1), 1)
         dn = int(mpmath.floor(10 * frac_part))
         return dn
